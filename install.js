64ef2a9bc641530001d537ad2a202c027a9f4c97
alex.zeng <595276686@qq.com>
2020-12-31 18:34:18 +0800

    feat: update codes

--- install.js
+++ install.js
@@ -1,19 +1,6 @@
-const { spawn } = require("child_process");
-const ls = spawn("yarn", [
-  "add",
-  "dayjs",
-  "antd-dayjs-webpack-plugin",
-  "lodash-webpack-plugin",
-]);
-
-ls.stdout.on("data", (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on("data", (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on("close", (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+const { execSync } = require("child_process");
+execSync(
+  "yarn",
+  ["add", "dayjs", "antd-dayjs-webpack-plugin", "lodash-webpack-plugin"],
+  { stdio: "inherit" }
+);
--- src/index.js
+++ src/index.js
@@ -10,18 +10,17 @@ function updateExternals(value) {
   return newParams;
 }
 
-export default function (api, options) {
-  const { cwd } = api;
-
+export default function(api, options) {
+  const { cwd, compatDirname } = api;
   // 用来自动配置外部script
-  (options?.externals || []).map((v) => {
+  options?.externals?.map(v => {
     return api.addHTMLHeadScript({
       content: v?.name,
-      src: v?.url,
+      src: v?.url
     });
   });
 
-  api.modifyAFWebpackOpts((memo) => {
+  api.modifyAFWebpackOpts(memo => {
     memo.extraBabelPlugins = [
       ...(memo.extraBabelPlugins || []),
       [
@@ -29,23 +28,30 @@ export default function (api, options) {
         {
           libraryName: "@antv/l7-react",
           libraryDirectory: "es/component",
-          camel2DashComponentName: false,
+          camel2DashComponentName: false
         },
-        "@antv/l7-react",
-      ],
+        "@antv/l7-react"
+      ]
     ];
-    console.log("memo modifyAFWebpackOpts after===>>>", memo);
     return memo;
   });
 
-  api.chainWebpackConfig((webpackConfig) => {
+  api.chainWebpackConfig(webpackConfig => {
+    webpackConfig.resolve.alias.set(
+      "dayjs",
+      compatDirname(
+        "dayjs/package.json",
+        cwd,
+        path.dirname(require.resolve("dayjs/package.json"))
+      )
+    );
     webpackConfig
       .plugin("lodash")
       .use("lodash-webpack-plugin", [{ paths: true }]);
     webpackConfig.plugin("moment2dayjs").use("antd-dayjs-webpack-plugin", [
       {
-        preset: "antdv3",
-      },
+        preset: "antdv3"
+      }
     ]);
     webpackConfig.externals(updateExternals(options.externals));
     webpackConfig.resolve.alias.set(
